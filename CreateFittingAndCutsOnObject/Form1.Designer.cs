cf1dabbdb08c431515c6b2f60b7a7893328d690d
Jack Devlin <40267278@ads.qub.ac.uk>
2021-10-12 15:59:28 +0100

    Added fittings and cuts project

---
+++ CreateFittingAndCutsOnObject/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace CreateFittingAndCutsOnObject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.beamBTN = new System.Windows.Forms.Button();
+            this.beamCutsBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // beamBTN
+            // 
+            this.beamBTN.Location = new System.Drawing.Point(306, 107);
+            this.beamBTN.Name = "beamBTN";
+            this.beamBTN.Size = new System.Drawing.Size(148, 75);
+            this.beamBTN.TabIndex = 0;
+            this.beamBTN.Text = "Create Beam with fittings";
+            this.beamBTN.UseVisualStyleBackColor = true;
+            this.beamBTN.Click += new System.EventHandler(this.beamBTN_Click);
+            // 
+            // beamCutsBTN
+            // 
+            this.beamCutsBTN.Location = new System.Drawing.Point(306, 273);
+            this.beamCutsBTN.Name = "beamCutsBTN";
+            this.beamCutsBTN.Size = new System.Drawing.Size(148, 75);
+            this.beamCutsBTN.TabIndex = 1;
+            this.beamCutsBTN.Text = "Create Beam with cuts";
+            this.beamCutsBTN.UseVisualStyleBackColor = true;
+            this.beamCutsBTN.Click += new System.EventHandler(this.beamCutsBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.beamCutsBTN);
+            this.Controls.Add(this.beamBTN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button beamBTN;
+        private System.Windows.Forms.Button beamCutsBTN;
+    }
+}
+
---
+++ CreateFittingAndCutsOnObject/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+using Tekla.Structures.Geometry3d;
+
+namespace CreateFittingAndCutsOnObject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void beamBTN_Click(object sender, EventArgs e)
+        {
+            Model model = new Model();
+
+            Beam myBeam = new Beam();
+            myBeam.StartPoint = new Point(0, 0, 0);
+            myBeam.EndPoint = new Point(1000, 0, 0);
+
+            myBeam.Name = "Beam";
+            myBeam.Material.MaterialString = "S235JR";
+            myBeam.Profile.ProfileString = "UB406*140*46";
+
+            myBeam.Insert();
+
+            Fitting Fitting = new Fitting();
+            Fitting.Plane = new Plane();
+            Fitting.Plane.Origin = new Point(500, 0, 0);
+            Fitting.Plane.AxisX = new Vector(0, 1000, 0);
+            Fitting.Plane.AxisY = new Vector(0, 0, -1000);
+            Fitting.Father = myBeam;
+            Fitting.Insert();
+
+            model.CommitChanges();
+            
+        }
+
+        private void beamCutsBTN_Click(object sender, EventArgs e)
+        {
+            Model model = new Model();
+
+            Beam myBeam = new Beam();
+            myBeam.StartPoint = new Point(0, 0, 0);
+            myBeam.EndPoint = new Point(1000, 0, 0);
+
+            myBeam.Name = "Beam";
+            myBeam.Material.MaterialString = "S235JR";
+            myBeam.Profile.ProfileString = "UB406*140*46";
+
+            myBeam.Insert();
+
+            CutPlane cutPlane = new CutPlane();
+            cutPlane.Plane = new Plane();
+            cutPlane.Plane.Origin = new Point(500, 0, 0);
+            cutPlane.Plane.AxisX = new Vector(0, 1000, 0);
+            cutPlane.Plane.AxisY = new Vector(0, 0, -1000);
+            cutPlane.Father = myBeam;
+            cutPlane.Insert();
+
+            model.CommitChanges();
+        }
+    }
+}
